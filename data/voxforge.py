155ab1cf258dd9959b787b2f66f59f2a6c9cfe10
vladimirwest <west957@mail.ru>
2018-11-07 01:39:20 +0300

    voxforge fix for russian dataset

--- data/voxforge.py
+++ data/voxforge.py
@@ -74,6 +74,10 @@ def prepare_sample(recording_name, url, target_folder):
                 if transcription_key not in transcriptions:
                     continue
                 utterance = transcriptions[transcription_key]
+				utterance = re.sub('[!,?#^()%.+-:;]', '', utterance)
+				utterance = re.sub('[  ]', ' ', utterance)
+				utterance = re.sub('[  ]', ' ', utterance)
+
 
                 target_wav_file = os.path.join(wav_dir, "{}_{}.wav".format(recording_name, recording_id))
                 target_txt_file = os.path.join(txt_dir, "{}_{}.txt".format(recording_name, recording_id))
